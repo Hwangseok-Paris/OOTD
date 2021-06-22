3de51ab84fdb5f0bd3609a0be15697fe44e284c9
Paris <Paris@DESKTOP-72D7A13>
2021-06-22 22:32:20 +0900

    2021-06-22 10:31 : SQL 추가

--- OOTD/SQL/0622_2230_SEQUENCE.sql
+++ OOTD/SQL/0622_2230_SEQUENCE.sql
@@ -1,8 +1,9 @@
 --------------------------------------------------------
---  파일이 생성됨 - 목요일-6월-17-2021   
+--  파일이 생성됨 - 화요일-6월-22-2021   
 --------------------------------------------------------
 DROP SEQUENCE "OOTD"."SEQ_ATT_NO";
 DROP SEQUENCE "OOTD"."SEQ_CART_NO";
+DROP SEQUENCE "OOTD"."SEQ_CART_TEMP_NO";
 DROP SEQUENCE "OOTD"."SEQ_MEMBER_NO";
 DROP SEQUENCE "OOTD"."SEQ_ORDER_NO";
 DROP SEQUENCE "OOTD"."SEQ_PRODUCT_NO";
@@ -11,12 +12,17 @@ DROP SEQUENCE "OOTD"."SEQ_REVIEW_NO";
 --  DDL for Sequence SEQ_ATT_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "OOTD"."SEQ_ATT_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "OOTD"."SEQ_ATT_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 161 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_CART_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "OOTD"."SEQ_CART_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 141 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "OOTD"."SEQ_CART_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 81 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CART_TEMP_NO
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OOTD"."SEQ_CART_TEMP_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 10020 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_MEMBER_NO
 --------------------------------------------------------
@@ -26,12 +32,12 @@ DROP SEQUENCE "OOTD"."SEQ_REVIEW_NO";
 --  DDL for Sequence SEQ_ORDER_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "OOTD"."SEQ_ORDER_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 367 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "OOTD"."SEQ_ORDER_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 407 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_PRODUCT_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "OOTD"."SEQ_PRODUCT_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "OOTD"."SEQ_PRODUCT_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_REVIEW_NO
 --------------------------------------------------------
